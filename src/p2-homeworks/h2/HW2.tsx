24e6aedfd8d72e57489ef55e8f84649feebacc2f
Tampoka <goal.thanks@gmail.com>
2021-09-25 14:55:17 +0900

    started typization

--- src/p2-homeworks/h2/HW2.tsx
+++ src/p2-homeworks/h2/HW2.tsx
@@ -2,12 +2,16 @@ import React, {useState} from 'react'
 import Affairs from './Affairs'
 
 // types
-export type AffairPriorityType = any // need to fix any
-export type AffairType = any // need to fix any
+export type AffairPriorityType = "high"|"middle"|"low"
+export type AffairType = {
+    _id:number
+    name:string
+    priority:AffairPriorityType
+}
 export type FilterType = 'all' | AffairPriorityType
 
 // constants
-const defaultAffairs: any = [ // need to fix any
+const defaultAffairs: Array<AffairType> = [
     {_id: 1, name: 'React', priority: 'high'},
     {_id: 2, name: 'anime', priority: 'low'},
     {_id: 3, name: 'games', priority: 'low'},
@@ -16,7 +20,7 @@ const defaultAffairs: any = [ // need to fix any
 ]
 
 // pure helper functions
-export const filterAffairs = (affairs: any, filter: any): any => { // need to fix any
+export const filterAffairs = (affairs: AffairType, filter: FilterType): any => { // need to fix any
     if (filter === 'all') return affairs
     else return // need to fix
 }
