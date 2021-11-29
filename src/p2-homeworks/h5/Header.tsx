5011a3dbd5fdc5245a8168764895ca3f009b3c3d
Tampoka <goal.thanks@gmail.com>
2021-11-29 11:38:21 +0900

    Add Links.

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from "react-router-dom"
 
 function Header() {
     return (
         <div>
-            // add NavLinks
+            <Link to='/'>Pre-Junior</Link>
+            <Link to='/junior'>Junior</Link>
+            <Link to='/junior-plus'>Junior+</Link>
 
         </div>
     )
