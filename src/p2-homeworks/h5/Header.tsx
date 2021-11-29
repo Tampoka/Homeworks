d8fc0716f6bdd223f0043742fc16547e407c496d
Tampoka <goal.thanks@gmail.com>
2021-11-29 13:46:50 +0900

    Add toggle button for nav links.

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,16 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Link} from 'react-router-dom'
 import {PATH} from './AppRoutes';
 import s from './Header.module.css'
 import style from '../../Btn.module.css'
 
 function Header() {
+    const [showNav, setShowNav]=useState(false)
+    const toggleShowNav=()=>setShowNav(!showNav)
+    const navClass=s.navContainer+' '+(showNav?s.visible:'')
     return (
-        <div className={s.navContainer}>
-            <><Link className={style.glowBtn} to='/'>Pre-Junior</Link>
+        <div className={s.header}>
+            <div className={navClass}>
+                <Link className={style.glowBtn} to='/'>Pre-Junior</Link>
                 <Link className={style.glowBtn} to={PATH.JUNIOR}>Junior</Link>
-                <Link className={style.glowBtn} to={PATH.JUNIOR_PLUS}>Junior+</Link></>
-            <button>?</button>
+                <Link className={style.glowBtn} to={PATH.JUNIOR_PLUS}>Junior+</Link>
+                <div>
+                    <button onClick={toggleShowNav}>?</button>
+                </div>
+            </div>
         </div>
     )
 }
