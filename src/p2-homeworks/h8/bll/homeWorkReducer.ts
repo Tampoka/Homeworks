240a945759bf8a2204564bb7e406a8160f2c11e8
Tampoka <goal.thanks@gmail.com>
2021-12-02 16:00:14 +0900

    Finish homeWorkReducer.

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,12 +1,19 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from "../HW8";
+
+export const homeWorkReducer = (state: UserType[], action: any): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': {
-            // need to fix
+          if (action.payload==='up') {
+              return [...state].sort((a, b) => a.name > b.name ? 1 : -1)
+          }
+            if (action.payload==='down') {
+                return [...state].sort((a, b) => a.name < b.name ? 1 : -1)
+            }
             return state
         }
         case 'check': {
+            return [...state].filter(u=>u.age>18)
             // need to fix
-            return state
         }
         default: return state
     }
