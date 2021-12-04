8c8864a877fdde15f53409aca34351cc05dbb3c6
Tampoka <goal.thanks@gmail.com>
2021-12-04 10:50:25 +0900

    Add login for loadingReducer.

--- src/p2-homeworks/h10/bll/loadingReducer.ts
+++ src/p2-homeworks/h10/bll/loadingReducer.ts
@@ -1,14 +1,24 @@
+export type InitStateType={
+    isLoading:boolean
+}
 const initState = {
-
+isLoading:false
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+const SET_LOADING="SET_LOADING"
+
+export const loadingReducer = (state:InitStateType = initState, action: LoadingActionType): InitStateType => { // fix any
     switch (action.type) {
-        case '': {
-            return state
+        case SET_LOADING: {
+            return {...state,isLoading:action.isLoading }
         }
         default: return state
     }
 }
 
-export const loadingAC = (): any => {} // fix any
+type LoadingActionType = {
+    type: typeof SET_LOADING
+    isLoading:boolean
+}
+
+export const loadingAC = (isLoading:boolean):LoadingActionType=> ({type:SET_LOADING,isLoading,} as const) // fix any
