ea80c3c1ee1e08424855c8999424dd4ca79d3760
Tampoka <goal.thanks@gmail.com>
2021-12-04 10:57:28 +0900

    Add setLoading.

--- src/p2-homeworks/h10/HW10.tsx
+++ src/p2-homeworks/h10/HW10.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import {useDispatch, useSelector} from "react-redux";
+import {AppStoreType} from "./bll/store";
+import {InitStateType, loadingAC} from "./bll/loadingReducer";
 
 function HW10() {
     // useSelector, useDispatch
-    const loading = false
+    const dispatch = useDispatch()
+    const {isLoading} = useSelector<AppStoreType, InitStateType>(state => state.isLoading)
 
     const setLoading = () => {
         // dispatch
         // setTimeout
+        dispatch(loadingAC(true))
+        setTimeout(()=>dispatch(loadingAC(false)),2000)
         console.log('loading...')
     };
 
@@ -17,7 +23,7 @@ function HW10() {
             homeworks 10
 
             {/*should work (должно работать)*/}
-            {loading
+            {isLoading
                 ? (
                     <div>крутилка...</div>
                 ) : (
