eec0f3e66f99cd3c62a3b8463a45a218e2d3cde4
Tampoka <goal.thanks@gmail.com>
2021-12-10 08:20:39 +0900

    Fix min-height.

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -10,21 +10,21 @@ type ActionType = {
 export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': {
-            if (action.payload === 'up') {
-                return [...state].sort((a, b) => a.name > b.name ? 1 : -1)
-            }
-            if (action.payload === 'down') {
-                return [...state].sort((a, b) => a.name < b.name ? 1 : -1)
-            }
-            return state
-        }
-/*
+            /*    if (action.payload === 'up') {
+                    return [...state].sort((a, b) => a.name > b.name ? 1 : -1)
+                }
+                if (action.payload === 'down') {
+                    return [...state].sort((a, b) => a.name < b.name ? 1 : -1)
+                }
+                return state*/
+
             const newState = [...state].sort((a, b) => {
                 if (a.name > b.name) return 1
                 else if (a.name < b.name) return -1
                 else return 0
             })
-            return action.payload === 'up' ? newState : newState.reverse()*/
+            return action.payload === 'up' ? newState : newState.reverse()
+        }
 
         case 'check': {
             return state.filter(u => u.age >= 18)
