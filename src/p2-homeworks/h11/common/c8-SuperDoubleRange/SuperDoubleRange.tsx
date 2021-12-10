dffb5fe295dcf0c53d12ee78cc303afe00483633
Tampoka <goal.thanks@gmail.com>
2021-12-10 22:27:21 +0900

    Add SuperDoubleRange.

--- src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
+++ src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
@@ -1,23 +1,33 @@
-import React from 'react'
+import React, {Dispatch, SetStateAction} from 'react'
+import {Box, Slider} from "@mui/material";
 
 type SuperDoubleRangePropsType = {
-    onChangeRange?: (value: [number, number]) => void
-    value?: [number, number]
-    // min, max, step, disable, ...
+    value?: number | number[]
+    // getAriaLabel: () => void
+    valueLabelDisplay: string
+    setRange:Dispatch<SetStateAction<number | number[]>>
 }
 
-const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
-    {
-        onChangeRange, value,
+const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = ({value,valueLabelDisplay,setRange,...restProps}
+        // onChangeRange, value,
         // min, max, step, disable, ...
-    }
 ) => {
     // сделать самому, можно подключать библиотеки
 
+    const handleChange = (event:Event, newValue:number | number[]) => {
+        setRange(newValue as any);
+    };
     return (
-        <>
+        <Box sx={{width: 300}}>
             DoubleRange
-        </>
+            <Slider
+                getAriaLabel={() => 'DoubleRange'}
+                value={value}
+                onChange={handleChange}
+                valueLabelDisplay="auto"
+                // getAriaValueText={valuetext}
+             />
+        </Box>
     )
 }
 
